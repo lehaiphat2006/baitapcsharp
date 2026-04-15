a046cbee556b959ddd75749342147a170ebd7eb6
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-04-15 13:26:42 +0700

    them file

---
+++ bai1.1/bai1.1/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Viết chương trình tính tiền điện phải trả dựa vào số kWh tiêu thụ và bảng giá.
+
+Bảng giá:
+Số kWh <= 100: 2000 đ/kWh
+Từ kWh thứ 101 đến 150: 2500 đ/kWh
+Từ kWh thứ 151 trở đi 3000 đ/kWh
+Nếu số kWh tiêu thụ vượt quá 300 thì cộng thêm 10% tiền phải trả.
+
+Input: Số kWh tiêu thụ
+Output: Số tiền phải thanh toán (VND)
+*/
+
+using System;
+
+namespace TinhTienDien
+{
+    class Program
+    {
+        // Khai báo các hằng 
+        int m1 = 100, m2 = 150, m3 = 300;
+        int g1 = 2000, g2 = 2500, g3 = 3000;
+        int kwh = 0;
+        double sotien = 0;
+        static void Main()
+        {
+
+            Program p = new Program();
+            Console.WriteLine("nhap so kwh:");
+            p.kwh = int.Parse(Console.ReadLine());
+            if (p.kwh <= p.m1)
+            {
+                p.sotien = p.kwh * p.g1;
+            }
+            else if (p.kwh <= p.m2)
+            {
+                p.sotien = p.m1 * p.g1 + (p.kwh - p.m1) * p.g2;
+            }
+            else if (p.kwh <= p.m3)
+            {
+                p.sotien = p.m1 * p.g1 + (p.m2 - p.m1) * p.g2 + (p.kwh - p.m2) * p.g3;
+            }
+            if (p.kwh > p.m3)
+            {
+                p.sotien = (p.m1 * p.g1 + (p.m2 - p.m1) * p.g2 + (p.m3 - p.m2) * p.g3 + (p.kwh - p.m3) * p.g3) * 1.1;
+            }
+            // Output
+            Console.WriteLine("So tien: " + p.sotien);
+        }
+    }
+}
--- bai4_2/bai4_2/obj/Release/net10.0/bai4_2.AssemblyInfo.cs
+++ bai4_2/bai4_2/obj/Release/net10.0/bai4_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bai4_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0682420d7f0b3bb330179654d17fd87deb0d88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add2a6a2cfa99a781d6046ddaee57d741eae3430")]
 [assembly: System.Reflection.AssemblyProductAttribute("bai4_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bai4_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
