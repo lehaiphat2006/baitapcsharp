40644067147c93bcce9ae33ed59312200b7964ae
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-05-24 17:52:12 +0700

    bai5.2

---
+++ bai5.2/bai5.2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace bai5._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblketqua = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.btngiai = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập hệ số a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập hệ số b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập hệ số c:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nghiệm của phương trình:";
+            // 
+            // lblketqua
+            // 
+            this.lblketqua.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblketqua.Location = new System.Drawing.Point(363, 36);
+            this.lblketqua.Name = "lblketqua";
+            this.lblketqua.Size = new System.Drawing.Size(191, 75);
+            this.lblketqua.TabIndex = 4;
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(135, 12);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 22);
+            this.txta.TabIndex = 5;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(135, 36);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 22);
+            this.txtb.TabIndex = 6;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(135, 60);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 22);
+            this.txtc.TabIndex = 7;
+            // 
+            // btngiai
+            // 
+            this.btngiai.Location = new System.Drawing.Point(16, 90);
+            this.btngiai.Name = "btngiai";
+            this.btngiai.Size = new System.Drawing.Size(87, 23);
+            this.btngiai.TabIndex = 8;
+            this.btngiai.Text = "Giải";
+            this.btngiai.UseVisualStyleBackColor = true;
+            this.btngiai.Click += new System.EventHandler(this.btngiai_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(135, 90);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(100, 23);
+            this.btnthoat.TabIndex = 9;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 129);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btngiai);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.lblketqua);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Giải phương trình bậc 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblketqua;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Button btngiai;
+        private System.Windows.Forms.Button btnthoat;
+    }
+}
+
---
+++ bai5.2/bai5.2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai5._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btngiai_Click(object sender, EventArgs e)
+        {
+            // 1. Kiểm tra và lấy dữ liệu từ các TextBox
+            if (double.TryParse(txta.Text, out double a) &&
+                double.TryParse(txtb.Text, out double b) &&
+                double.TryParse(txtc.Text, out double c))
+            {
+                // 2. Tiến hành giải phương trình
+                if (a == 0)
+                {
+                    // Nếu a = 0, phương trình trở thành bậc 1: bx + c = 0
+                    if (b == 0)
+                    {
+                        if (c == 0)
+                            lblketqua.Text = "Phương trình có vô số nghiệm";
+                        else
+                            lblketqua.Text = "Phương trình vô nghiệm";
+                    }
+                    else
+                    {
+                        double x = -c / b;
+                        lblketqua.Text = $"Phương trình bậc 1 có nghiệm x = {Math.Round(x, 4)}";
+                    }
+                }
+                else
+                {
+                    // Nếu a != 0, tính Delta
+                    double delta = b * b - 4 * a * c;
+
+                    if (delta < 0)
+                    {
+                        lblketqua.Text = "Phương trình vô nghiệm";
+                    }
+                    else if (delta == 0)
+                    {
+                        double x = -b / (2 * a);
+                        lblketqua.Text = $"Phương trình có nghiệm kép x1 = x2 = {Math.Round(x, 4)}";
+                    }
+                    else
+                    {
+                        double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                        double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                        // Hiển thị kết quả 
+                        lblketqua.Text = $"Phương trình có 2 nghiệm x1 = {Math.Round(x1, 4)}/ x2 = {Math.Round(x2, 4)}";
+                    }
+                }
+            }
+            else
+            {
+                // Thông báo nếu người dùng nhập chữ hoặc để trống
+                MessageBox.Show("Vui lòng nhập đầy đủ các hệ số a, b, c dưới dạng số!", "Lỗi nhập liệu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có muốn thoát không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
