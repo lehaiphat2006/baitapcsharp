2e1f28df2c3a8f996a2f04a14041eae38131306b
lehaiphat2006 <lehaiphat2006@gmail.com>
2026-05-26 12:03:04 +0700

    plappy bird

---
+++ bai5.6/bai5.6/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace bai5._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBird = new System.Windows.Forms.PictureBox();
+            this.picGround = new System.Windows.Forms.PictureBox();
+            this.picPipedown = new System.Windows.Forms.PictureBox();
+            this.picPipe = new System.Windows.Forms.PictureBox();
+            this.lbscore = new System.Windows.Forms.Label();
+            this.gametime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGround)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPipedown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPipe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBird
+            // 
+            this.picBird.Image = ((System.Drawing.Image)(resources.GetObject("picBird.Image")));
+            this.picBird.Location = new System.Drawing.Point(28, 235);
+            this.picBird.Name = "picBird";
+            this.picBird.Size = new System.Drawing.Size(73, 64);
+            this.picBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBird.TabIndex = 0;
+            this.picBird.TabStop = false;
+            // 
+            // picGround
+            // 
+            this.picGround.Image = ((System.Drawing.Image)(resources.GetObject("picGround.Image")));
+            this.picGround.Location = new System.Drawing.Point(-1, 473);
+            this.picGround.Name = "picGround";
+            this.picGround.Size = new System.Drawing.Size(806, 77);
+            this.picGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGround.TabIndex = 1;
+            this.picGround.TabStop = false;
+            // 
+            // picPipedown
+            // 
+            this.picPipedown.Image = ((System.Drawing.Image)(resources.GetObject("picPipedown.Image")));
+            this.picPipedown.Location = new System.Drawing.Point(266, -127);
+            this.picPipedown.Name = "picPipedown";
+            this.picPipedown.Size = new System.Drawing.Size(100, 307);
+            this.picPipedown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPipedown.TabIndex = 2;
+            this.picPipedown.TabStop = false;
+            // 
+            // picPipe
+            // 
+            this.picPipe.Image = ((System.Drawing.Image)(resources.GetObject("picPipe.Image")));
+            this.picPipe.Location = new System.Drawing.Point(511, 334);
+            this.picPipe.Name = "picPipe";
+            this.picPipe.Size = new System.Drawing.Size(100, 144);
+            this.picPipe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPipe.TabIndex = 3;
+            this.picPipe.TabStop = false;
+            // 
+            // lbscore
+            // 
+            this.lbscore.BackColor = System.Drawing.SystemColors.Info;
+            this.lbscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbscore.Location = new System.Drawing.Point(3, 499);
+            this.lbscore.Name = "lbscore";
+            this.lbscore.Size = new System.Drawing.Size(635, 34);
+            this.lbscore.TabIndex = 4;
+            this.lbscore.Text = "Score:";
+            // 
+            // gametime
+            // 
+            this.gametime.Interval = 20;
+            this.gametime.Tick += new System.EventHandler(this.gametime_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(626, 553);
+            this.Controls.Add(this.lbscore);
+            this.Controls.Add(this.picPipe);
+            this.Controls.Add(this.picPipedown);
+            this.Controls.Add(this.picGround);
+            this.Controls.Add(this.picBird);
+            this.Name = "Form1";
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGround)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPipedown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPipe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBird;
+        private System.Windows.Forms.PictureBox picGround;
+        private System.Windows.Forms.PictureBox picPipedown;
+        private System.Windows.Forms.PictureBox picPipe;
+        private System.Windows.Forms.Label lbscore;
+        private System.Windows.Forms.Timer gametime;
+    }
+}
+
---
+++ bai5.6/bai5.6/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai5._6
+{
+    public partial class Form1 : Form
+    {
+        // Khởi tạo các biến quản lý trạng thái game
+        int pipeSpeed = 8;      // Tốc độ di chuyển của ống cống
+        int gravity = 5;        // Lực hút kéo chim rơi xuống
+        int score = 0;          // Điểm số ban đầu
+        bool gameOver = false;  // Trạng thái kết thúc game
+        public Form1()
+        {
+            InitializeComponent();
+            // Bật thuộc tính KeyPreview để Form nhận được sự kiện phím trước các control khác
+            this.KeyPreview = true;
+
+            // Bắt đầu chạy Timer khi mở game
+            gametime.Start();
+        }
+        // Hàm xử lý khi thua
+        private void EndGame()
+        {
+            gametime.Stop(); // Dừng bộ đếm thời gian (game đóng băng)
+            gameOver = true;
+            lbscore.Text += " Game over! Press R to restart.";
+        }
+        // Hàm reset game để chơi lại
+        private void RestartGame()
+        {
+            gameOver = false;
+            picBird.Top = 150;        // Đưa chim về vị trí ban đầu
+            picPipedown.Left = 500; // Đưa ống về vị trí ban đầu
+            score = 0;
+            gravity = 5;
+            lbscore.Text = "Score: 0";
+            gametime.Start();        // Chạy lại timer
+        }
+        // Sự kiện xảy ra sau mỗi vòng lặp của Timer (mỗi 20ms)
+        private void gametime_Tick(object sender, EventArgs e)
+        {
+            // 1. Cho chim rơi xuống liên tục theo trọng lực
+            picBird.Top += gravity;
+
+            // 2. Di chuyển ống cống sang bên trái
+            picPipedown.Left -= pipeSpeed;
+            picPipe.Left -= pipeSpeed;
+
+            // 3. Hiển thị điểm số hiện tại
+            lbscore.Text = "Score: " + score;
+
+            // 4. Nếu ống cống chạy khuất màn hình bên trái, reset nó về bên phải để tạo vòng lặp
+            if (picPipedown.Left < -50)
+            {
+                picPipedown.Left = 500; // Đẩy ống về lại bên phải ngoài màn hình
+                score++;                  // Người chơi vượt qua và cộng 1 điểm
+            }
+            if (picPipe.Left < -50)
+            {
+                picPipe.Left = 500; // Đẩy ống về lại bên phải ngoài màn hình
+                score++;                  // Người chơi vượt qua và cộng 1 điểm
+            }
+
+            // 5. Kiểm tra va chạm (Thua cuộc)
+            // Chim chạm vào ống cống HOẶC chạm mặt đất HOẶC bay quá biên trên
+            if (picBird.Bounds.IntersectsWith(picPipedown.Bounds) ||
+                picBird.Bounds.IntersectsWith(picGround.Bounds) ||
+                picBird.Bounds.IntersectsWith(picPipe.Bounds)||
+                picBird.Top < 0)
+            {
+                EndGame();
+            }
+        }
+        // Sự kiện khi người chơi NHẤN phím (Spacebar để nhảy)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Nếu bấm phím Space (Dấu cách) thì đổi hướng trọng lực để chim bay lên
+            if (e.KeyCode == Keys.Space && !gameOver)
+            {
+                gravity = -10; // Lực nhảy lên
+            }
+        }
+        // Sự kiện khi người chơi THẢ phím ra
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Khi thả phím Space ra thì chim lại bị rơi xuống tiếp
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5; // Trả lại lực rơi xuống
+            }
+
+            // Nếu game đã over, bấm R để chơi lại từ đầu
+            if (e.KeyCode == Keys.R && gameOver)
+            {
+                RestartGame();
+            }
+        }
+    }
+}
